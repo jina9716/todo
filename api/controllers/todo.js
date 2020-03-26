3156aaea093c76d9b9248d481b7fdc69edfdec7e
jina9716 <jina9716@gmail.com>
2020-03-26 16:29:30 +0900

    수정

--- api/controllers/todo.js
+++ api/controllers/todo.js
@@ -1,23 +1,70 @@
 'use strict';
 
+const co = require('co');
 const service = require('../services/todo');
 
 exports.createToDo = function(req, res){
-    return service.createToDo(req, res);
+    service.createToDo(req.body)
+    .then(()=>{
+        return res.status(204).send();
+    })
+    .catch(error=>{
+        return res.status(400).send(error);
+    });
 };
 
-exports.getToDoList = function(req, res){
-    return service.getToDoList(req, res);
+exports.createToDo_co = function(req, res){
+    co(function* (){
+        try {
+            yield service.createToDo(req.body);
+            return res.status(204).send();
+        } catch (error) {
+            return res.status(400).send(error);
+        }
+    });
 };
 
-exports.getToDo = function(req, res){
-    return service.getToDo(req, res);
+exports.createToDo_async = async function(req, res){
+    try {
+        await service.createToDo(req.body);
+        return res.status(204).send();
+    } catch (error) {
+        return res.status(400).send(error);   
+    }
 };
 
-exports.updateToDo = function(req, res){
-    return service.updateToDo(req, res);
+exports.getToDoList = async function(req, res){
+    try {
+        let result = await service.getToDoList(req.query);
+        return res.send(result);
+    } catch (error) {
+        return res.status(400).send(error);
+    }
 };
 
-exports.deleteToDo = function(req, res){
-    return service.deleteToDo(req, res);
+exports.getToDo = async function(req, res){
+    try {
+        let result = await service.getToDo(req.params.todoId);
+        return res.send(result);
+    } catch (error) {
+        return res.status(400).send(error);
+    }
+};
+
+exports.updateToDo = async function(req, res){
+    try {
+        await service.updateToDo(req.params.todoId, req.body);
+        return res.status(204).send();
+    } catch (error) {
+        return res.status(400).send(error);
+    }
+};
+
+exports.deleteToDo = async function(req, res){
+    try {
+        await service.deleteToDo(req.params.todoId);
+        return res.status(204).send();
+    } catch (error) {
+        return res.status(400).send(error);
+    }
 };
