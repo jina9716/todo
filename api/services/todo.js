c11d03ee6a43932c8e1e2685c3e3b63f25d86686
jina9716 <jina9716@gmail.com>
2020-03-27 17:30:43 +0900

    오류 수정 및 소스 변경

--- api/services/todo.js
+++ api/services/todo.js
@@ -7,16 +7,7 @@ exports.createToDo = function(data){
 };
 
 exports.getToDoList = async function(params){
-    const title = params.title;
-    const status = params.status;
-    const context = params.context;
-    const startDueDate = params.startDueDate;
-    const endDueDate = params.endDueDate;
-    const startDoneAt = params.startDoneAt;
-    const endDoneAt = params.endDoneAt ;
-    const startCreatedAt = params.startCreatedAt;
-    const endCreatedAt = params.endCreatedAt;
-    const page = params.skip;
+    const {title, status, context, startDueDate, endDueDate, startDoneAt, endDoneAt, startCreatedAt, endCreatedAt, page} = params;
 
     if (new Date(startDueDate) > new Date(endDueDate) || new Date(startDoneAt) > new Date(endDoneAt) || new Date(startCreatedAt) > new Date(endCreatedAt)) {
         throw "조회 종료일은 조회 시작일보다 빠를 수 없습니다.";
@@ -50,14 +41,14 @@ exports.getToDoList = async function(params){
     }
 
     const totalCount = await query.countDocuments();
-    const list = await query.find().select('title status context dueDate createdAt doneAt -_id').skip(Number(page)).limit(10).exec();
+    const list = await query.find().skip((Number(page)-1)*10).limit(10).exec();
 
     const result = {items: list, totalCount: totalCount};
     return result;
 };
 
 exports.getToDo = function(todoId){
-    return Todo.findById(todoId).select('title status context dueDate createdAt doneAt -_id').exec();
+    return Todo.findById(todoId).exec();
 };
 
 exports.updateToDo = function(todoId, data){
